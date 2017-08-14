329057648596ab1da129d8566446c52680c891f3
floriico <floriico@gmail.com>
2017-08-14 23:00:08 +0200

    Create required index.html create folder for js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+(function () {
+  var canvas = document.querySelector('canvas');
+  var ctx = canvas.getContext('2d');
+
+  ctx.fillStyle = '#123';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+})();
