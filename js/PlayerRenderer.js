8492627901ac96fcabf2c58cab248d998cadc4a8
floriico <floriico@gmail.com>
2017-09-12 22:21:04 +0200

    Add UI for inventory

--- js/PlayerRenderer.js
+++ js/PlayerRenderer.js
@@ -1,6 +1,7 @@
 function PlayerRenderer (options) {
   this.player = options.player;
   this.canvas = options.canvas;
+  this.sprites = options.sprites;
 }
 
 PlayerRenderer.prototype.render = function () {
@@ -12,4 +13,25 @@ PlayerRenderer.prototype.render = function () {
 
   ctx.fillStyle = '#fabbec';
   ctx.fillRect(center.x - 5, center.y - 20, 10, 20);
+  this.renderInventory();
 };
+
+PlayerRenderer.prototype.renderInventory = function () {
+  var ctx = this.canvas.getContext('2d');
+  var offsetY = Math.floor(this.canvas.height / 5);
+
+  ctx.font = '10px sans-serif';
+  ctx.fillStyle = Colors.white;
+  ctx.fillText(this.player.transmitter ? '1 x' : '0 x', this.canvas.width - 50, offsetY + 10);
+  ctx.drawImage(this.sprites.spriteSheet,
+      this.sprites.ids.transmitter.offset.x, this.sprites.ids.transmitter.offset.y, 16, 16,
+      this.canvas.width - 32, offsetY, 16, 16);
+  ctx.fillText(this.player.antenna ? '1 x' : '0 x', this.canvas.width - 50, offsetY + 32 + 10);
+  ctx.drawImage(this.sprites.spriteSheet,
+      this.sprites.ids.antenna.offset.x, this.sprites.ids.antenna.offset.y, 16, 16,
+      this.canvas.width - 32, offsetY + 32, 16, 16);
+  ctx.fillText(this.player.battery ? '1 x' : '0 x', this.canvas.width - 50, offsetY + 64 + 10);
+  ctx.drawImage(this.sprites.spriteSheet,
+      this.sprites.ids.battery.offset.x, this.sprites.ids.battery.offset.y, 16, 16,
+      this.canvas.width - 32, offsetY + 64, 16, 16);
+}
